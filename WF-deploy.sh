c48c78a8cdef9901190cde6824ed553b2b71cfd2
rawaqasha <r.qasha@newcastle.ac.uk>
2016-01-28 13:45:09 +0000

    first commit

---
+++ WF-deploy.sh
@@ -0,0 +1,22 @@
+wf=$1
+#!/bin/bash
+
+sudo service docker start
+
+echo "deploy the workflow"
+if [[ ${wf} == 1 ]]; then
+   if [[ -d ~/realeScWF-inhost ]]; then
+      echo "previous workflow execution exists and will be deleted"
+      rm -r ~/realeScWF-inhost
+   fi
+  cfy local init --install-plugins -p blueprint/realeScWF-inhost.yaml -i input.yaml
+else
+   if [[ -d ~/realeScWF-1host ]]; then
+      echo "previous workflow execution exists and will be deleted"
+      rm -r ~/realeScWF-1host
+   fi
+  cfy local init --install-plugins -p blueprint/realeScWF-1host.yaml -i input.yaml
+fi
+
+
+cfy local execute -w install
---
+++ wf-install.sh
@@ -0,0 +1,45 @@
+WF=$1  #blueprint type 1:container per task, 2:container per WF
+
+#install wget
+Wget=$(which wget)
+#echo "${Dock}"
+if [[ -z ${Wget} ]]; then
+   echo "wget installation"
+   sudo apt-get install -y wget
+fi
+
+# docker installation
+Dock=$(which docker)
+echo "${Dock}"
+if [[ -z ${Dock} ]]; then
+   echo "Docker installation"
+   if [ ! -f temp-install/docker-installation.sh ]; then
+      wget -O temp-install/docker-installation.sh https://github.com/rawaqasha/Workflows-Repository/raw/master/docker-installation.sh
+      chmod u+x TOSCA-WFDS/docker-installation.sh
+   fi
+   ./temp-install/docker-installation.sh
+fi
+
+#Cloudify configuration and installation
+
+if [[ ! -d ~/WF-Cloudify ]]; then
+   echo "Cloudify installation"
+   wget -O temp-install/cloudify-install.sh https://github.com/rawaqasha/Workflows-Repository/raw/master/cloudify-install.sh
+   chmod u+x ~temp-install/cloudify-install.sh
+   . ./temp-install/cloudify-install.sh
+else 
+   source ~/WF-Cloudify/bin/activate
+fi
+
+
+#workflow deployment
+echo "workflow deployment"
+
+chmod u+x WF-deploy.sh
+if [[ ${WF} == 1 ]]; then
+   echo "NJ deployment with Multiple Containers"
+   .WF-deploy.sh 1
+else
+   echo "NJ deployment with Single Container"
+   .WF-deploy.sh 2
+fi
