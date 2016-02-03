710df4ae0471dba3326d15052d31ce9af9693da7
rawaqasha <r.qasha@newcastle.ac.uk>
2016-02-03 12:22:59 +0000

    tested realeScWF-1host

--- NJ-deploy.sh
+++ NJ-deploy.sh
@@ -23,13 +23,13 @@ echo "deploy the workflow"
 if [[ ${wf} == 1 ]]; then
    if [[ -d ~/realeScWF-inhost ]]; then
       echo "previous workflow execution exists and will be deleted"
-      rm -r ~/realeScWF-inhost
+      rm -rf ~/realeScWF-inhost
    fi
   cfy local init --install-plugins -p realeScWF-inhost.yaml --input input.yaml
 else
    if [[ -d ~/realeScWF-1host ]]; then
       echo "previous workflow execution exists and will be deleted"
-      rm -r ~/realeScWF-1host
+      rm -rf ~/realeScWF-1host
    fi
   cfy local init --install-plugins -p realeScWF-1host.yaml -i input.yaml
 fi
