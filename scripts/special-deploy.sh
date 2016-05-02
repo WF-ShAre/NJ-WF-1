9d027ad94f67a3c1a641e66830e47ce60030e649
Rawaa Qasha <r.qasha@newcastle.ac.uk>
2016-05-02 11:55:12 +0100

    Update special-deploy.sh

--- scripts/special-deploy.sh
+++ scripts/special-deploy.sh
@@ -11,6 +11,12 @@ sudo docker exec -it ${CONTAINER_ID} [ ! -d tasks ] && sudo docker exec -it ${CO
 
 sudo docker exec -it ${CONTAINER_ID} [ ! -f tasks/${BLOCK_NAME} ] && sudo docker exec -it ${CONTAINER_ID} wget -O tasks/${BLOCK_NAME} ${BLOCK_URL}
 
+var=$(echo ${BLOCK_NAME} | cut -f 1 -d '.')
+image=${var,,}
+ctx logger info "Creating ${image}"
+if [[ "$(docker images -q ${image} 2> /dev/null)" = "" && $blueprint = "realeScWF-inhost" ]]; then
+   sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} dtdwd/${image}
+fi
 
 ctx logger info "Execute the block"
 if [ $block = "Mega-NJ" ]; then
