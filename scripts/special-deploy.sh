0ec532eac0c50659831ec2f04fefeba8872844a7
DTDWD <r.qasha@newcastle.ac.uk>
2016-05-05 17:26:03 +0100

    working with Core script

--- scripts/special-deploy.sh
+++ scripts/special-deploy.sh
@@ -11,10 +11,9 @@ sudo docker exec -it ${CONTAINER_ID} [ ! -d tasks ] && sudo docker exec -it ${CO
 
 sudo docker exec -it ${CONTAINER_ID} [ ! -f tasks/${BLOCK_NAME} ] && sudo docker exec -it ${CONTAINER_ID} wget -O tasks/${BLOCK_NAME} ${BLOCK_URL}
 
-var=$(echo ${BLOCK_NAME} | cut -f 1 -d '.')
-image=${var,,}
-ctx logger info "Creating ${image}"
-if [[ "$(docker images -q ${image} 2> /dev/null)" = "" && $blueprint = "realeScWF-inhost" ]]; then
+image=$(echo ${BLOCK_NAME} | cut -f 1 -d '.')
+if [[ "$(docker images -q dtdwd/${image} 2> /dev/null)" = "" && $blueprint = "realeScWF-inhost" ]]; then
+   ctx logger info "Creating ${image}"
    sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} dtdwd/${image}
 fi
 
