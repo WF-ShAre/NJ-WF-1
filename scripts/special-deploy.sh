d043ddde54c546bdfde8d10702523d4616542bf7
DTDWD <r.qasha@newcastle.ac.uk>
2016-05-23 15:19:12 +0100

    working version with create image choice

--- scripts/special-deploy.sh
+++ scripts/special-deploy.sh
@@ -3,19 +3,7 @@ blueprint=$1
 block=$(ctx node name)
 CONTAINER_ID=$2
 BLOCK_NAME=$(ctx node properties block_name)
-BLOCK_URL=$3
-LIB_DIR=$4
-
-
-sudo docker exec -it ${CONTAINER_ID} [ ! -d tasks ] && sudo docker exec -it ${CONTAINER_ID} mkdir tasks
-
-sudo docker exec -it ${CONTAINER_ID} [ ! -f tasks/${BLOCK_NAME} ] && sudo docker exec -it ${CONTAINER_ID} wget -O tasks/${BLOCK_NAME} ${BLOCK_URL}
-
-image=$(echo ${BLOCK_NAME} | cut -f 1 -d '.')
-if [[ "$(docker images -q dtdwd/${image} 2> /dev/null)" = "" && $blueprint = "realeScWF-inhost" ]]; then
-   ctx logger info "Creating ${image}"
-   sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} dtdwd/${image}
-fi
+LIB_DIR=$3
 
 ctx logger info "Execute the block"
 if [ $block = "Mega-NJ" ]; then
